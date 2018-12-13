b8855ae6275adc74960969d77b3d394eda424a51
Emanuele Beccaria <34138916+altspin92@users.noreply.github.com>
2018-12-13 12:26:48 +0100

    Create Calcolatrice simpatica.py

---
+++ Calcolatrice simpatica.py
@@ -0,0 +1,25 @@
+#Programma scritto da Emanuele Beccaria emanuelebeccaria1992@gmail.
+#Calcolatrice simpatica
+#Per ogni dubbio o consigli non esitate a contattarmi, sono un neofita nella programmazione!
+#Presentazione calcolatrice
+print("Ciao, il mio nome è calcolatore, e sono qui per guidarti a fare i tuoi calcoli")
+input("")
+nome = input("Per prima cosa dimmi come ti chiami...  ")
+print("benvenuto ", nome, "sono molto felice di fare la tua conoscenza")
+#calcoli da fare: moltiplicazione, divisione, addizione, sottrazione, potenze
+input("per iniziare premere invio")
+print(".......................................")
+x = input("inserisci il primo numero ")
+y = input("inserisci il secondo numero ")
+addizione = float(x) + float(y)
+sottrazione = float(x) - float(y)
+moltiplicazione = float(x) * float(y)
+divisione = float(x) / float(y)
+potenza = float(x) ** float(y)
+print("il risultato dell'addizione dovrebbe essere: ", addizione)
+print("il risultato della sottrazione dovrebbe essere: ", sottrazione)
+print("il risultato della moltiplicazione dovrebbe essere: ", moltiplicazione)
+print("il risultato della divisione dovrebbe essere: ", divisione)
+print("il risultato della potenza dovrebbe essere: ", potenza)
+input("Preme un tasto")
+print("Grazie di avermi usato \nMi evolvo continuamente per poter essere sempre più potente e facile da usare")
